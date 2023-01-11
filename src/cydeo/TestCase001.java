6bda4ee36fbd7aad874f9687ef2767642ff7b6a6
syavuzer <yavuzercolorado@hotmail.com>
2023-01-11 10:57:57 -0700

    added message

--- src/cydeo/TestCase001.java
+++ src/cydeo/TestCase001.java
@@ -4,5 +4,9 @@ public class TestCase001 {
     /*
     This current state is status of the project up to now
      */
+    public static void main(String[] args) {
+        System.out.println("Practice");
+    }
+
 
 }
